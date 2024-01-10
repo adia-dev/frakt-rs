348f5632bf3e0faccf1066da4ca4eb7b0269f66b
Abdoulaye Dia <abdoudu78130@gmail.com>
2024-01-10 12:11:57 +0100

    💄 Made the cli help commands for Server and Worker pretty
    e.g: `cargo run -- server --help`

--- cli/src/commands/worker/mod.rs
+++ cli/src/commands/worker/mod.rs
@@ -1,16 +1,35 @@
 use clap::Parser;
 
+/// 👷 Worker Command
+/// 
+/// This command configures and launches a worker instance 🚧.
+/// Workers perform specific tasks 🛠️ assigned by the server.
 #[derive(Parser, Debug)]
+#[command(name = "worker", about = "🚀 Start and configure a worker.", long_about = None)]
 pub struct WorkerCommand {
-    #[arg(short, long)]
+    /// 📛 Worker name
+    /// 
+    /// Unique identifier for the worker 🆔.
+    /// If not set, a random name will be generated.
+    #[arg(short, long, value_name = "NAME")]
     pub name: Option<String>,
 
-    #[arg(short, long)]
+    /// 📡 Server address
+    /// 
+    /// The IP address of the server 🏢 to which the worker will connect.
+    #[arg(short, long, value_name = "ADDRESS")]
     pub address: Option<String>,
 
-    #[arg(short, long)]
+    /// 🔌 Server port
+    /// 
+    /// The port number of the server 🎚️ to which the worker will connect.
+    #[arg(short, long, value_name = "PORT")]
     pub port: Option<u16>,
 
-    #[arg(long)]
+    /// 🏋️‍♂️ Maximum workload
+    /// 
+    /// Define the maximum workload that the worker can handle 📊.
+    /// This could be in terms of tasks, computations, or data size.
+    #[arg(long, value_name = "WORKLOAD")]
     pub maximal_work_load: Option<u32>,
 }
